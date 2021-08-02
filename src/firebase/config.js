87fe39e3daeb08cd243df13cd64e28b3a412d20d
preetchani <preetchani23498@gmail.com>
2021-08-02 19:33:56 +0530

    First Commit

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAn3yeLr1G6UVvRIDjqkfcSaEhr7y59Q2w",
+    authDomain: "react-firegram-bf620.firebaseapp.com",
+    projectId: "react-firegram-bf620",
+    storageBucket: "react-firegram-bf620.appspot.com",
+    messagingSenderId: "215177272230",
+    appId: "1:215177272230:web:6bba44d67bcdcdc5950d67"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFireStorage = firebase.firestore();
+
+  export {projectStorage,projectFireStorage};
